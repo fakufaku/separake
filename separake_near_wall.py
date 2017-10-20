e2c5e30bd60cf484fcd501d29cc13723f466d568
Robin Scheibler <fakufaku@gmail.com>
2017-10-21 00:18:58 +0900

    Started to get separake_near_wall in parallel form

--- separake_near_wall.py
+++ separake_near_wall.py
@@ -5,8 +5,90 @@ import pyroomacoustics as pra
 
 from multinmf_conv_mu import multinmf_conv_mu_wrapper
 from utilities import partial_rir, reverse_simulate
+from sim_tools import json_append
 
 from mir_eval.separation import bss_eval_images
+
+base_dir = os.path.abspath(os.path.split(__file__)[0])
+print('Base dir', base_dir)
+
+if not os.path.exists('data'):
+    os.mkdir('data')
+
+# output filename format. {} is replaced by date/time
+data_dir_format = base_dir + '/data/{}_mu_near_wall'  # The {} is replaced with date
+data_file_format = 'data_{}.json'  # The {} is replaced by node pid
+param_file_format = '/parameters.json'  # We store the parameters in a json file
+args_file_format = '/args.json'  # We store the arguments list in a json file
+error_file_format = '/error_{}.json'  # We store some debug info on failed instances
+
+# parameters
+parameters = dict(
+    fs = 16000,
+
+    # room parameters
+    max_order = 8,  # max image sources order in simulation
+    floorplan = [[0, 0], [6, 0], [6, 5], [2,5], [0,3]],
+    height = 4.,
+    absorption = 0.4,
+    mics_locs = [ [ 5.50, 5.50, 5.50 ],
+                   [ 4.53, 4.55, 4.57 ],
+                   [ 0.70, 0.70, 0.70 ] ],
+
+    n_epochs = 3  # number of trials for each parameters combination
+
+    # convolutive separation parameters
+    dictionary_file = 'W_dictionary_sqmag.npz',
+    mu_n_iter = 200,        # number of iterations of MU algorithm
+    stft_win_len = 2048,    # supposedly optimal at 16 kHz (Ozerov and Fevote 2010)
+    use_dict = True,
+    mu_n_latent_var = 4,    # number of latent variables (ignored when dictionary is used)
+    )
+
+# parameters to sweep
+partial_lengths = list(range(0,11,2))  # number of image sources to use in the 'raking'
+l1_reg = [1e-2,1e-3,1e-4] # only used with a dictionary, automatically set to zero otherwise
+seeds = np.random.randint(2**32, size=n_epochs)
+args = itertools.product(partial_lengths, l1_reg, seeds)
+
+def parallel_loop(partial_length, gamma, result_file=None,
+        stft_win_len=None, fs=None, room=None, src_signals=None, mic_signals=None,
+        mu_n_latent_var=None, W_dict=None, mu_n_iter=None, seed=None, **kwargs):
+    ''' This the function that should be dumb parallel '''
+
+    # compute partial rir
+    freqvec = np.fft.rfftfreq(stft_win_len, 1 / fs)
+    partial_rirs = partial_rir(room, partial_length + 1, freqvec)
+
+    partial_rirs_sources = np.swapaxes(
+            np.array( [pr for i,pr in enumerate(partial_rirs) if src_signals[i] is not None]),
+            0, 1)
+
+    # separate using MU
+    sep_sources = multinmf_conv_mu_wrapper(
+            mic_signals.T, partial_rirs_sources, 
+            mu_n_latent_var, W_dict=W_dict, l1_reg=gamma, 
+            n_iter=mu_n_iter, verbose=False, random_seed=seed)
+
+    # compute the metrics
+    n_samples = np.minimum(single_sources.shape[1], sep_sources.shape[1])
+    ret = \
+            bss_eval_images(single_sources[:,:n_samples,:], sep_sources[:,:n_samples,:])
+
+    entry = dict(
+            partial_length=partial_length,
+            gamma=gamma,
+            seed=seed,
+            sdr=ret[0].tolist(),
+            isr=ret[1].tolist(),
+            sir=ret[2].tolist(),
+            sar=ret[3].tolist(),
+            )
+
+    json_append(result_file, entry)
+
+    return entry
+
 
 if __name__ == '__main__':
 
@@ -15,22 +97,6 @@ if __name__ == '__main__':
     has been designed to work with image sources, a simple hack is to reverse the roles
     of sources and microphones.
     '''
-
-    # parameters
-    fs = 16000
-    nfft = 2048  # supposedly optimal at 16 kHz (Ozerov and Fevote)
-    max_order = 8  # max image sources order in simulation
-
-    partial_lengths = list(range(0,11,2))  # number of image sources to use in the 'raking'
-    n_epochs = 1          # number of epochs for simulation
-
-    # convolutive separation parameters
-    mu_n_latent_var = 4    # number of latent variables in the NMF
-    mu_n_iter = 200        # number of iterations of MU algorithm
-    stft_win_len = 2048    # supposedly optimal at 16 kHz
-    use_dict = True
-    l1_reg = [1e-3] # only used with a dictionary, automatically set to zero otherwise
-    opt_verbose = True
 
     # the speech samples
     r1, speech1 = wavfile.read('data/Speech/fq_sample1.wav')
@@ -41,9 +107,12 @@ if __name__ == '__main__':
         raise ValueError('The speech samples should have the same sample rate as the simulation')
 
     # a 5 wall room
-    floorplan = np.array([[0, 0], [6, 0], [6, 5], [2,5], [0,3]]).T
-    room = pra.Room.from_corners(floorplan, fs=fs, absorption=0.4, max_order=max_order)
-    room.extrude(4., absorption=0.4)  # add the third dimension
+    room = pra.Room.from_corners(np.array(parameters['floorplan']).T,
+                                 fs=parameters['fs'], 
+                                 absorption=parameters['absorption'],
+                                 max_order=parameters['max_order'])
+    # add the third dimension
+    room.extrude(parameters['height'], absorption=parameters['absorption'])
 
     # add two sources
     K = 10  # number of sources
@@ -55,9 +124,7 @@ if __name__ == '__main__':
     room.add_microphone_array(source_array)
 
     # now add a few microphones
-    mics_locs = np.array([ [ 5.50, 5.50, 5.50 ],
-                           [ 4.53, 4.55, 4.57 ],
-                           [ 0.70, 0.70, 0.70 ] ])
+    mics_locs = np.array(parameters['mics_locs'])
     for m in range(mics_locs.shape[1]):
         room.add_source(mics_locs[:,m])
 
@@ -68,7 +135,7 @@ if __name__ == '__main__':
     src_signals = [None] * K
     src_signals[0] = speech1
     src_signals[K-1] = speech2
-    signals = reverse_simulate(room, src_signals)
+    mic_signals = reverse_simulate(room, src_signals)
 
     # simulate also the sources separately for comparison
     single_sources = []
@@ -77,7 +144,7 @@ if __name__ == '__main__':
             continue
         feed = [None] * K
         feed[i] = s
-        single_sources.append(reverse_simulate(room, feed, length=signals.shape[1]))
+        single_sources.append(reverse_simulate(room, feed, length=mic_signals.shape[1]))
     single_sources = np.swapaxes(np.array(single_sources), 1, 2)
 
     # prepare the dictionary
@@ -105,29 +172,6 @@ if __name__ == '__main__':
 
             for g, gamma in enumerate(l1_reg):
                 print('  Number of image microphones:', partial_length)
-
-                # compute partial rir
-                freqvec = np.fft.rfftfreq(nfft, 1 / fs)
-                partial_rirs = partial_rir(room, partial_length + 1, freqvec)
-
-                partial_rirs_sources = np.swapaxes(
-                        np.array( [pr for i,pr in enumerate(partial_rirs) if src_signals[i] is not None]),
-                        0, 1)
-
-                # separate using MU
-                sep_sources = multinmf_conv_mu_wrapper(
-                        signals.T, partial_rirs_sources, 
-                        mu_n_latent_var, W_dict=W_dict, l1_reg=gamma, 
-                        n_iter=mu_n_iter, verbose=opt_verbose, random_seed=seed)
-
-                # compute the metrics
-                n_samples = np.minimum(single_sources.shape[1], sep_sources.shape[1])
-                ret = \
-                        bss_eval_images(single_sources[:,:n_samples,:], sep_sources[:,:n_samples,:])
-                scores['sdr'][index,g,epoch] = np.mean(ret[0])
-                scores['isr'][index,g,epoch] = np.mean(ret[1])
-                scores['sir'][index,g,epoch] = np.mean(ret[2])
-                scores['sar'][index,g,epoch] = np.mean(ret[3])
 
 
     # plot the results
